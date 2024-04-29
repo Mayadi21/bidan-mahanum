82e137b5da9ccac3c6d6d6e0829a3c9ffc6a3d44
Refael Siagian <juarisiagian21106@gmail.com>
2024-04-29 10:09:51 +0700

    Fondasi tubes

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -11,7 +11,7 @@
                 <img src="https://source.unsplash.com/500x250?category_slug" class="card-img" alt="category">
                 <div class="card-img-overlay d-flex align-items-center p-0">
                 <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.5)">
-                    <a href="/categories/category" class="text-decoration-none text-white">Category</a>
+                    <a href="/categories/category_slug" class="text-decoration-none text-white">Category</a>
                 </h5>
                 </div>
             </div>
@@ -22,7 +22,7 @@
                 <img src="https://source.unsplash.com/500x250?category_slug" class="card-img" alt="category">
                 <div class="card-img-overlay d-flex align-items-center p-0">
                 <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.5)">
-                    <a href="/categories/category" class="text-decoration-none text-white">Category</a>
+                    <a href="/categories/category_slug" class="text-decoration-none text-white">Category</a>
                 </h5>
                 </div>
             </div>
@@ -32,7 +32,7 @@
                 <img src="https://source.unsplash.com/500x250?category_slug" class="card-img" alt="category">
                 <div class="card-img-overlay d-flex align-items-center p-0">
                 <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.5)">
-                    <a href="/categories/category" class="text-decoration-none text-white">Category</a>
+                    <a href="/categories/category_slug" class="text-decoration-none text-white">Category</a>
                 </h5>
                 </div>
             </div>
