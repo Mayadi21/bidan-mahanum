c8033e3e2d1ffba26db20c153f5679bd283494f6
jessicaaa <jesicamaha36@gmail.com>
2024-05-02 20:48:21 +0700

    add category

--- resources/views/categories.blade.php
+++ resources/views/categories.blade.php
@@ -6,36 +6,18 @@
 
     <div class="row">
         {{-- Loop here --}}
+        @foreach($categories as $category)
         <div class="col-md-4">
             <div class="card text-bg-dark mb-3">
-                <img src="https://source.unsplash.com/500x250?category_slug" class="card-img" alt="category">
+                <img src="https://source.unsplash.com/500x250?{{ $category->category_slug }}" class="card-img" alt="{{ $category->category_name }}">
                 <div class="card-img-overlay d-flex align-items-center p-0">
-                <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.5)">
-                    <a href="/categories/category_slug" class="text-decoration-none text-white">Category</a>
-                </h5>
+                    <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.5)">
+                        <a href="{{ route('category.show', $category->category_slug) }}" class="text-decoration-none text-white">{{ $category->category_name }}</a>
+                    </h5>
                 </div>
             </div>
         </div>
-        {{-- Loop here --}}
-        <div class="col-md-4">
-            <div class="card text-bg-dark mb-3">
-                <img src="https://source.unsplash.com/500x250?category_slug" class="card-img" alt="category">
-                <div class="card-img-overlay d-flex align-items-center p-0">
-                <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.5)">
-                    <a href="/categories/category_slug" class="text-decoration-none text-white">Category</a>
-                </h5>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4">
-            <div class="card text-bg-dark mb-3">
-                <img src="https://source.unsplash.com/500x250?category_slug" class="card-img" alt="category">
-                <div class="card-img-overlay d-flex align-items-center p-0">
-                <h5 class="card-title text-center flex-fill p-4 fs-3" style="background-color: rgba(0, 0, 0, 0.5)">
-                    <a href="/categories/category_slug" class="text-decoration-none text-white">Category</a>
-                </h5>
-                </div>
-            </div>
-        </div>
+        @endforeach
+        {{-- Loop end --}}
     </div>
 @endsection
